a52b445478a03fea5321ec0fe325e921037b8b8f
Pablo del Pino <pablo.delpino@beanworks.com>
2020-03-12 14:12:20 -0800

    Test application v1

---
+++ app/dbconfig.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import preprocessing
+mysql = {'MYSQL_DATABASE_HOST': 'db',
+         'MYSQL_DATABASE_USER': 'MYUSER',
+         'MYSQL_DATABASE_PASSWORD': 'MYPASSWORD',
+         'MYSQL_DATABASE_DB': 'mydb'}
--- app/main.py
+++ app/main.py
@@ -1,22 +1,26 @@
 from flask import Flask
-from flaskext.mysql import MySQL
+from flaskext.mysql import MySQL 
+from configparser import ConfigParser 
 
+# config = ConfigParser()  
+# config.read('database.ini')  
 
 mysql = MySQL()
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'MYUSER'
+app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'MYPASSWORD'
 app.config['MYSQL_DATABASE_DB'] = 'mydb'
-app.config['MYSQL_DATABASE_HOST'] = 'MYDBHOST'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_PORT'] = '3306'
 mysql.init_app(app)
 
 @app.route("/message")
 def message():
-    cursor = mysql.connect().cursor()
+    cursor = mysql.connect.connect().cursor()
     cursor.execute("SELECT message from mytable where 1")
     return cursor.fetchone()
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
