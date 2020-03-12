c3006bd6f8c1cb73a15e4fafd2fc74ef7d310e9a
Pablo del Pino <pablo.delpino@beanworks.com>
2020-03-12 14:42:15 -0800

    Test application v2

--- app/dbconfig.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import preprocessing
-mysql = {'MYSQL_DATABASE_HOST': 'db',
-         'MYSQL_DATABASE_USER': 'MYUSER',
-         'MYSQL_DATABASE_PASSWORD': 'MYPASSWORD',
-         'MYSQL_DATABASE_DB': 'mydb'}
--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 from flask import Flask
 from flaskext.mysql import MySQL 
-from configparser import ConfigParser 
-
-# config = ConfigParser()  
-# config.read('database.ini')  
+from os import environ
 
 mysql = MySQL()
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'MYPASSWORD'
-app.config['MYSQL_DATABASE_DB'] = 'mydb'
-app.config['MYSQL_DATABASE_HOST'] = 'db'
-app.config['MYSQL_DATABASE_PORT'] = '3306'
+app.config['MYSQL_DATABASE_USER'] = environ.get('MYSQL_DATABASE_USER') 
+app.config['MYSQL_DATABASE_PASSWORD'] = environ.get('MYSQL_DATABASE_PASSWORD') 
+app.config['MYSQL_DATABASE_DB'] = environ.get('MYSQL_DATABASE_DB') 
+app.config['MYSQL_DATABASE_HOST'] = environ.get('MYSQL_DATABASE_HOST') 
+app.config['MYSQL_DATABASE_PORT'] = environ.get('MYSQL_DATABASE_PORT') 
 mysql.init_app(app)
 
 @app.route("/message")
