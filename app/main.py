9449dc0a7ef01241c68f50dc0185ba57bac387a4
Pablo del Pino <pablo.delpino@beanworks.com>
2020-03-12 18:49:23 -0800

    Final commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flaskext.mysql import MySQL 
-from mysql import connector
 import config
 
 mysql = MySQL()
@@ -10,13 +9,10 @@ mysql.init_app(app)
 
 @app.route("/message")
 def message():
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
+    cursor = mysql.connect().cursor()
     cursor.execute("SELECT message from mytable where 1")
-    cursor.close()
-    connection.close()
     return cursor.fetchone()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="app", debug=True)
